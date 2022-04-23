02fe0580916d264fbf6dc76ac4f99bec62466361
MaureenDadap <maureendadap@gmail.com>
2022-04-23 09:12:48 +0800

    fixed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 window.hashtagWallConfig = {
-  hashtag: '#NULagunaOpenDay2022',
+  hashtag: '#BrandNUJourney',
   apiBaseUrl: 'http://localhost:8080',
   // apiBaseUrl: 'https://nu-open-day-2022-api.loca.lt/',
   updateInterval: 1000,
